1648f36af3186d37573e604e01415e75c6672e83
jawad224 <jawadsheikh224@gmail.com>
2021-11-01 01:05:02 +0500

    pull subhan

---
+++ src/Client/pages/delete.js
@@ -0,0 +1,85 @@
+<section className="circle-1">
+                <div className="container pb-5 position-relative">
+                  <div id="demo" className="carousel slide position-relative" data-ride="carousel">
+                    {/* Indicators */}
+                    <ul className="carousel-indicators">
+                      <li data-target="#demo" data-slide-to={0} className />
+                      <li data-target="#demo" data-slide-to={1} className="active" />
+                      <li data-target="#demo" data-slide-to={2} className />
+                    </ul>
+                    {/* The slideshow */}
+                    <div className="carousel-inner">
+                      <div className="carousel-item active carousel-item-left">
+                        <div className="d-flex">
+                          <div className="col-lg-12 col-sm-12 col-12 d-flex flex-column ">
+                            <a href="#" className="introduce pb-3">
+                              <div className="flex-column-1 ">
+                                <p className="pr-2 pl-3 pt-4">What a Service!!!!!Antony is one of the top Classy designer in the
+                                  world.Exceeded expectation and designed my website to my requirments.Will be usingfor
+                                  ongoing work.</p>
+                              </div>
+                              <div className="flex-column-2 d-flex">
+                                <div className="pic p1" />
+                                <div className="information  flex-column pl-3 pt-2">
+                                  <p>David Beckham</p>
+                                  <p className="text-gray">Managing Director</p>
+                                </div>
+                              </div>
+                              <span />
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="carousel-item carousel-item-next carousel-item-left">
+                        <div className="d-flex">
+                          <div className="col-lg-12 col-sm-12 col-12 d-flex flex-column ">
+                            <a href="#" className="introduce pb-3">
+                              <div className="flex-column-1 ">
+                                <p className="pr-2 pl-3 pt-4">What a Service!!!!!Antony is one of the top Classy designer in the
+                                  world.Exceeded expectation and designed my website to my requirments.Will be usingfor
+                                  ongoing work.</p>
+                              </div>
+                              <div className="flex-column-2 d-flex">
+                                <div className="pic p1" />
+                                <div className="information  flex-column pl-3 pt-2">
+                                  <p>David Luiz</p>
+                                  <p className="text-gray">Ceo &amp; Founder</p>
+                                </div>
+                              </div>
+                              <span />
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="carousel-item ">
+                        <div className="d-flex">
+                          <div className="col-lg-12 col-sm-12 col-12 d-flex flex-column ">
+                            <a href="#" className="introduce pb-3">
+                              <div className="flex-column-1 ">
+                                <p className="pr-2 pl-3 pt-4">What a Service!!!!!Antony is one of the top Classy designer in the
+                                  world.Exceeded expectation and designed my website to my requirments.Will be usingfor
+                                  ongoing work.</p>
+                              </div>
+                              <div className="flex-column-2 d-flex">
+                                <div className="pic p1" />
+                                <div className="information  flex-column pl-3 pt-2">
+                                  <p>Donna Karan</p>
+                                  <p className="text-gray">Fashion Designer</p>
+                                </div>
+                              </div>
+                              <span />
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    {/* Left and right controls */}
+                    <a className="carousel-control-prev" href="#demo" data-slide="prev" style={{ opacity: 1, left: '-10%' }}>
+                      <i className="fas fa-chevron-left" style={{ color: '#5969f6', fontSize: '30px' }} />
+                    </a>
+                    <a className="carousel-control-next" href="#demo" data-slide="next" style={{ opacity: 1, transform: 'rotate(-180deg)', right: '-10%' }}>
+                      <i className="fas fa-chevron-left" style={{ color: '#5969f6', fontSize: '30px' }} />
+                    </a>
+                  </div>
+                </div>
+              </section>
